0cb31964f43f710eb87f8609ce448287325ddb74
AJuarez2023001 <HansAlexanderMerucry@gmail.com>
2023-02-25 15:06:06 -0600

    Ejercicio Ciclo

---
+++ Siclos.py
@@ -0,0 +1,18 @@
+
+#a = True
+#while a == True:
+     #print("Estoy en Verdadero")
+
+
+#x = 0
+#num =int(input("Imgrese un numero"))
+#while x < num:
+   # x = x + 1
+ #   print(x)
+
+animeles = ["Perro","Gato","Tucanes","Girafa","Cebra"]
+num = 0
+print(animeles)
+for x in animeles:
+    num = num + 1
+    print(str(num)+x)
